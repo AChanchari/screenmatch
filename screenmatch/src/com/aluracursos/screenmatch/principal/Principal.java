775c477ba8398696f27ce9f23c58bae63838cc75
AChanchari <ajchg30@gmail.com>
2025-05-09 15:08:20 -0500

    Proyecto avanzado

---
+++ screenmatch/src/com/aluracursos/screenmatch/principal/Principal.java
@@ -0,0 +1,71 @@
+package com.aluracursos.screenmatch.principal;
+
+import com.aluracursos.screenmatch.calculos.CalculadoraDeTiempo;
+import com.aluracursos.screenmatch.calculos.FiltroRecomendacion;
+import com.aluracursos.screenmatch.modelos.Episodio;
+import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
+
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula("Encanto", 2021);
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        miPelicula.evalua(7.8);
+        System.out.println(miPelicula.getTotalDeLasEvaluaciones());
+        System.out.println(miPelicula.calculaMedia());
+
+        Serie casaDragon = new Serie("La casa del dragón",2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodios(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+        casaDragon.muestraFichaTecnica();
+        System.out.println(casaDragon.getDuracionEnMinutos());
+
+        Pelicula otraPelicula = new Pelicula("Avatar", 2023);
+        otraPelicula.setNombre("Matrix");
+        otraPelicula.setFechaDeLanzamiento (1998);
+        otraPelicula.setDuracionEnMinutos(180);
+
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+        calculadora.incluye(miPelicula);
+        calculadora.incluye(casaDragon);
+        calculadora.incluye(otraPelicula);
+        System.out.println("Tiempo necesario para ver tus videos favoritos en estas vacaciones "+ calculadora.getTiempoTotal() + " minutos");
+
+
+        FiltroRecomendacion filtroRecomendacion = new FiltroRecomendacion();
+        filtroRecomendacion.filtra(miPelicula);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setNombre("La casa Targaryen");
+        episodio.setSerie(casaDragon);
+        episodio.setTotalVisualizaciones(50);
+        filtroRecomendacion.filtra(episodio);
+
+        var peliculaDeBruno = new Pelicula("El señor de los anillos", 2001);
+        peliculaDeBruno.setDuracionEnMinutos(180);
+        peliculaDeBruno.setFechaDeLanzamiento(2021);
+
+        ArrayList<Pelicula> listaDePelicula = new ArrayList<>();
+        listaDePelicula.add(peliculaDeBruno);
+        listaDePelicula.add(miPelicula);
+        listaDePelicula.add(otraPelicula);
+
+        System.out.println("Tamaño de la lista: " + listaDePelicula.size());
+        System.out.println("La primera pelicula es: " + listaDePelicula.get(0).getNombre());
+        System.out.println(listaDePelicula.toString());
+        System.out.println("toString de la pelicula: "+ listaDePelicula.get(0).toString());
+
+
+    }
+}
